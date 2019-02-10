14f4dec9c0d40d884cf2d99a39e7810ba064084c
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-02-10 20:13:43 +0100

    Add empty MortalEnemyList component

---
+++ mortal-enemy-list-frontend/src/MortalEnemyList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './MortalEnemyList.scss'
+
+class MortalEnemyList extends Component {
+  render() {
+    return (
+      <div className="mortal-enemy-list">
+        <h1>Mortal enemy list</h1>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default MortalEnemyList;

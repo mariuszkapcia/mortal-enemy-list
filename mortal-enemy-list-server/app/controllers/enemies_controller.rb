140d4577e8f95aab49c0f2c62428abe8d38b0ac9
Mariusz Kapcia <m.kapcia@nomtek.com>
2019-02-10 21:32:54 +0100

    Implement endpoint for add new enemies

--- mortal-enemy-list-server/app/controllers/enemies_controller.rb
+++ mortal-enemy-list-server/app/controllers/enemies_controller.rb
@@ -1,5 +1,22 @@
-class EnemiesController < ApplicationController
+class EnemiesController < ApiController
   def index
     render json: [{id: 'id', name: 'name'}], status: :ok
+  end
+
+  def create
+    command_bus.call(
+      Enemies::PutEnemyOnList.new(
+        enemy_id: params[:id],
+        name:     params[:name]
+      )
+    )
+
+    head :no_content
+  end
+
+  private
+
+  def command_bus
+    Rails.configuration.command_bus
   end
 end
